d6fce3256c3e9ffe2adfebdc8ac83591cac86deb
Shubh Sheth <shubhsheth8@gmail.com>
2022-02-26 17:08:54 -0600

    Add watchlist button in list item

--- src/app/media/media-list-item.js
+++ src/app/media/media-list-item.js
@@ -1,13 +1,19 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import mediaUtil from '../utils/media';
+import watchlist from '../utils/watchlist';
 
 const MediaListItem = ({item}) => {
     const [media, setMedia] = useState({});
+    const [inWatchlist, setInWatchlist] = useState(false);
 
     useEffect(() => {
         setMedia(mediaUtil.formatData(item));
     }, [item]);
+    
+    useEffect(() => {
+        setInWatchlist(watchlist.isInWatchlist(media.id, media.type));
+    }, [media]);
 
     const navigate = useNavigate();
     const goToMedia = () => {
@@ -21,8 +27,17 @@ const MediaListItem = ({item}) => {
         }
     }
 
+    const toggleWatchlist = () => {
+        if (inWatchlist) {
+            watchlist.removeFromWatchlist(media.id, media.type);
+        } else {
+            watchlist.addToWatchlist(media.id, media.type, media.title, media.poster_path, media.year, media.language);
+        }
+        setInWatchlist(!inWatchlist);
+    }
+
     return (
-        <div className="search__results__list__item" key={media.id}>
+        <div className="search__results__list__item media-list-item" key={media.id}>
             <div className="search__results__list__item__poster">
                 <img src={media.poster_path} alt={media.title} onClick={goToMedia} />
             </div>
@@ -32,9 +47,9 @@ const MediaListItem = ({item}) => {
                     <span className="metadata_item lang">{media.language}</span>
                     <span className="metadata_item date">{media.year}</span>
                 </div>
-                <div className="result_actions">
-                    <button className="btn btn-primary" onClick={shareURL}>Recommend</button>
-                    <button className="btn btn-primary">Add to List</button>
+                <div className="actions">
+                    <button className="action_button recommend" onClick={shareURL}>Recommend</button>
+                    <button className="action_button watchlist" onClick={toggleWatchlist}>{!inWatchlist ? "ADD" : "REMOVE"}</button>
                 </div>
             </div>
         </div>
