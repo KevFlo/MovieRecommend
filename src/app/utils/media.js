c49288df479ee41d4914a2426f415acb4ff5128c
Shubh Sheth <shubhsheth8@gmail.com>
2022-02-26 14:39:28 -0600

    Update Media Utils

--- src/app/utils/media.js
+++ src/app/utils/media.js
@@ -3,13 +3,13 @@ const mediaUtil = {
         data.media_type = media_type || data.media_type;
         const media = {
             id: data.id,
-            type: media_type || data.media_type,
-            title: (data.media_type === 'movie') ? data.title : data.name,
-            poster_path: (data.poster_path) ? `https://image.tmdb.org/t/p/w500${data.poster_path}` : 'https://via.placeholder.com/500x281',
-            backdrop_path: (data.backdrop_path) ? `https://image.tmdb.org/t/p/w500${data.backdrop_path}` : 'https://via.placeholder.com/500x750',
-            year: (data.media_type === 'movie') ? data.release_date?.substring(0, 4) : data.first_air_date?.substring(0, 4),
+            type: data.type || data.media_type,
+            title: data.title || data.name,
+            poster_path: data.poster || `https://image.tmdb.org/t/p/w500${data.poster_path}` || 'https://via.placeholder.com/500x750',
+            backdrop_path: (data.backdrop_path) ? `https://image.tmdb.org/t/p/w500${data.backdrop_path}` : 'https://via.placeholder.com/500x281',
+            year: data.year || data.release_date?.substring(0, 4) || data.first_air_date?.substring(0, 4),
             vote_average: data.vote_average,
-            language: data.original_language,
+            language: data.language || data.original_language,
         }
         return media;
     }
