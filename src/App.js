62d8ff63289efa7a455b60a9414f0d449d2de31d
Shubh Sheth <shubhsheth8@gmail.com>
2022-02-26 12:27:35 -0600

    Remove duplicate object

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Search from './app/search/search';
 import Dashboard from './app/dashboard/dashboard';
 import Media from './app/media/media';
-import Recommend from './app/recommendation/recommendation';
 import Nav from './app/nav/nav';
 import { Routes, Route } from "react-router-dom";
 
@@ -19,8 +18,6 @@ function App() {
           <Route path="/search" element={<Search query={searchQuery} setQuery={setSearchQuery} />} />
           <Route path="/tv/:id" element={<Media />} />
           <Route path="/movie/:id" element={<Media />} />
-          <Route path="recommend/tv/:id" element={<Recommend />} />
-          <Route path="recommend/movie/:id" element={<Recommend />} />
         </Routes>
       </div>
       <Nav />
