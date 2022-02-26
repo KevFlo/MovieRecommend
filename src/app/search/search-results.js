1a8c9751700f1e34a7e569aa307b1838a7d78508
Shubh Sheth <shubhsheth8@gmail.com>
2022-02-26 12:25:28 -0600

    Filter search results

--- src/app/search/search-results.js
+++ src/app/search/search-results.js
@@ -1,12 +1,15 @@
 import SearchResultItem from "./result-item";
 
 const SearchResults = ({results}) => {
+
+    const relevantResults = results.filter(result => result.media_type === 'movie' || result.media_type === 'tv');
+
     return (
         <div className="search__results">
             <div className="search__results__list">
                 {
-                    (results.length > 0) ? (
-                        results.map(result => (
+                    (relevantResults.length > 0) ? (
+                        relevantResults.map(result => (
                             <SearchResultItem key={result.id} item={result} />
                         ))
                     ) : (
