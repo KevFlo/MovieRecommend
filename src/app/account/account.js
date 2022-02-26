d43f82f67c428b66ae33c03ffbb63cbe46b837cb
Shubh Sheth <shubhsheth8@gmail.com>
2022-02-26 14:39:56 -0600

    Add watchlist on account page

--- src/app/account/account.js
+++ src/app/account/account.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+import watchlist from "../utils/watchlist";
+import MediaListItem from "../media/media-list-item";
+
 const Account = () => {
+    const [mediaList, setMediaList] = useState([]);
+
+    useEffect(() => {
+        const data = watchlist.getWatchlist();
+        setMediaList(data);
+    }, []);
+
     return (
         <div>
-        <h1>Account</h1>
+            <h2>Watchlist</h2>
+            <div className="media_list">
+                {
+                    mediaList.map((media, index) => {
+                        return (
+                            <MediaListItem key={index} item={media} />
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
